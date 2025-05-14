65e7adc73d885b57acd177ed157ee0a5cfabafc3
Jacob <jaclucb2.0@gmail.com>
2025-05-14 15:20:55 +1000

    commie

---
+++ login.php
@@ -0,0 +1,24 @@
+<?php
+session_start()
+
+$host = "localhost";
+$username = "root";
+$password = "";
+$database = "user.db";
+
+
+$conn = mysqli_connect($host, $username, $password, $database);
+
+$username = trim($_POST['username']);
+$password = trim($_POST['password']);
+
+// Simple query to check credentials
+$query = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
+$result = mysqli_query($conn, $query);
+$user = mysqli_fetch_assoc($result);
+
+if ($user["username"] == $user["username"]) {
+    header("Location: profile.php");
+
+
+?>
