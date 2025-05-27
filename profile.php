163a096afd27945744db6e6e7162af9e1a4bf69b
Jacob <jaclucb2.0@gmail.com>
2025-05-27 17:26:46 +1000

    work?

--- profile.php
+++ profile.php
@@ -2,6 +2,11 @@
 require_once "settings.php";
 session_start();
 
+$username = trim($_POST['username']);
+$_SESSION['username'] = $username;
+echo"Welcome, " . $_SESSION["username"];
+echo" " . $_SESSION["email"];
+
 if ($username) {
     $conn = mysqli_connect($host, $username, $password, $database);
 }
