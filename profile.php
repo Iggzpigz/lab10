83496aa63c0e3f8e205f0985a70ec5bc09f99cbb
Jacob <jaclucb2.0@gmail.com>
2025-05-27 17:32:26 +1000

    this fix?

--- profile.php
+++ profile.php
@@ -6,6 +6,8 @@ if ($username) {
     $conn = mysqli_connect($host, $username, $password, $database);
 }
 
+$query = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
+$result = mysqli_query($conn, $query);
 $user  = mysqli_fetch_assoc($result);
 
 if (!$user) {
