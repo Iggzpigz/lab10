89a88f8c1712171eabd8a6736934ebe3797fd9fb
Jacob <jaclucb2.0@gmail.com>
2025-05-27 19:23:59 +1000

    wil this work

--- profile.php
+++ profile.php
@@ -1,21 +1,8 @@
 <?php
 require_once "settings.php";
 session_start();
-
-if ($username) {
-    $conn = mysqli_connect($host, $username, $password, $database);
-}
-
-$query = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
-$result = mysqli_query($conn, $query);
-$user  = mysqli_fetch_assoc($result);
-
-if (!$user) {
-    $username = trim($_POST['username']);
-    $_SESSION['username'] = $username;
-    echo"Welcome, " . $_SESSION['username'];
-    echo" " . $_SESSION['email'];
-}
+$_SESSION['username'] = 'john'
+echo"Welcome" . $_SESSION['username'];
 
 
 
