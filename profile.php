7d29fe0c4a9ca70433a3882fa87255d56826407e
Jacob <jaclucb2.0@gmail.com>
2025-05-27 19:31:53 +1000

    will this work please please please please please

--- profile.php
+++ profile.php
@@ -1,9 +1,20 @@
 <?php
 require_once "settings.php";
 session_start();
-$_SESSION['username'] = 'john';
-echo "Welcome" . $_SESSION['username'];
 
+$conn = mysqli_connect($host, $username, $password, $database);
+
+if ($conn) {
+    $query = "SELECT * FROM users"
+    $result = mysqli_query($conn, $query);
+    if ($result) {}
+    else {}
+    mysqli_close($conn);
+} else echo"<p> Unable to connect to the database";
+while ($row = mysqli_fetch_assoc($result)) {
+    echo "<p> Welcome". $row["username"] ."</p>";
+    echo "<p>". $row["email"] ."</p>";
+}
 
 
 
